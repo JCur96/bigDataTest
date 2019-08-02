ec6e2cb2d0f95e2f9f9b2dd8fd79d4a84c9bcc64
JCur96 <jake.curry96@gmail.com>
2019-08-02 15:35:51 +0100

    Updated version number

--- R/demo.R
+++ R/demo.R
@@ -1,11 +1,12 @@
-#' #' demo.R
-#' #' this is to get working and generally demo the 'big data' capabilities of
-#' #' datastorr, as part of the CMEE MRES
-#'
-#' #' #' Check to see if its empty as it should be on release
-#' bigDataTest::mydata_versions()
-#' #' it is, so move on to getting the first version out there
-#' bigDataTest::mydata_release('Test release', 'IUCN_Spatial.csv')
-#'
-#'
-#' df <- read.csv('IUCN_Spatial.csv', header = T)
+#' demo.R
+#' this is to get working and generally demo the 'big data' capabilities of
+#' datastorr, as part of the CMEE MRES
+
+#' #' Check to see if its empty as it should be on release
+bigDataTest::mydata_versions()
+#' it is, so move on to getting the first version out there
+updateVersion()
+bigDataTest::mydata_release('Test release', 'IUCN_Spatial.csv')
+
+
+df <- read.csv('IUCN_Spatial.csv', header = T)
