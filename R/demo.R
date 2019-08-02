8fb2829db3c20ce2d6f565ce981c61725940b796
JCur96 <jake.curry96@gmail.com>
2019-08-02 14:57:11 +0100

    First commit

---
+++ R/demo.R
@@ -0,0 +1,8 @@
+#' demo.R
+#' this is to get working and generally demo the 'big data' capabilities of
+#' datastorr, as part of the CMEE MRES
+
+#' Check to see if its empty as it should be on release
+bigDataTest::mydata_versions()
+#' it is, so move on to getting the first version out there
+bigDataTest::mydata_release('Test release', 'IUCN_Spatial.csv')
