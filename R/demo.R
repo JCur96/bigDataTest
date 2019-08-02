946677142250b0d21e0cc784f94cb37a6d682d98
JCur96 <jake.curry96@gmail.com>
2019-08-02 14:59:30 +0100

    Added demo file to make datastorr happy

--- R/demo.R
+++ R/demo.R
@@ -2,7 +2,7 @@
 #' this is to get working and generally demo the 'big data' capabilities of
 #' datastorr, as part of the CMEE MRES
 
-#' Check to see if its empty as it should be on release
-bigDataTest::mydata_versions()
-#' it is, so move on to getting the first version out there
-bigDataTest::mydata_release('Test release', 'IUCN_Spatial.csv')
+#' #' Check to see if its empty as it should be on release
+#' bigDataTest::mydata_versions()
+#' #' it is, so move on to getting the first version out there
+#' bigDataTest::mydata_release('Test release', 'IUCN_Spatial.csv')
